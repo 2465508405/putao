1119b92ce2bb83db43bf3bff33338407316b1171
ykk <yangkuokuo@glz8.com>
2018-06-02 18:51:31 +0800

    commit

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -45,7 +45,7 @@ class HomeController extends Controller
         $categories = Category::where('base_id',1)->where('id','!=',$category->id)->orderBy('number','desc')->limit(3)->get();
         $articles = Article::where('status',3)->where('category_id',$id)->orderBy('created_at','desc')->paginate($pageSize);
         $pageSize = PageUtil::getPage($page,$articles->total(),$pageSize,$id,'s');
-        // dd($pageSize);
+//         dd($pageSize);
         return view('home.list',['category'=>$category,'categories'=>$categories,'articles'=>$articles,'pageSize'=>$pageSize]);
     }
 
