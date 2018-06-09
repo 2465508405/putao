384a4589c8c7113dec52ca4575a0d7d1ac1a2bc1
ykk <yangkuokuo@glz8.com>
2018-06-09 10:54:29 +0800

    commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,26 +7,26 @@
 	<link rel="stylesheet" href="/css/base.css">
 	<script src="/js/jquery.min.js"></script>
 	<!--文字滚动 start-->
-	<script type="text/javascript">
-		var speed=40
-		window.onload=function(){
-			var demo=document.getElementById("demo");
-			var demo2=document.getElementById("demo2");
-			var demo1=document.getElementById("demo1");
-			demo2.innerHTML=demo1.innerHTML
-			function Marquee(){
-				if(demo.scrollTop>=demo1.offsetHeight){
-					demo.scrollTop=0;
-				}
-				else{
-					demo.scrollTop=demo.scrollTop+1;
-				}
-			}
-			var MyMar=setInterval(Marquee,speed)
-			demo.onmouseover=function(){clearInterval(MyMar)}
-			demo.onmouseout=function(){MyMar=setInterval(Marquee,speed)}
-		}
-	</script>
+	{{--<script type="text/javascript">--}}
+		{{--var speed=40--}}
+		{{--window.onload=function(){--}}
+			{{--var demo=document.getElementById("demo");--}}
+			{{--var demo2=document.getElementById("demo2");--}}
+			{{--var demo1=document.getElementById("demo1");--}}
+			{{--demo2.innerHTML=demo1.innerHTML--}}
+			{{--function Marquee(){--}}
+				{{--if(demo.scrollTop>=demo1.offsetHeight){--}}
+					{{--demo.scrollTop=0;--}}
+				{{--}--}}
+				{{--else{--}}
+					{{--demo.scrollTop=demo.scrollTop+1;--}}
+				{{--}--}}
+			{{--}--}}
+			{{--var MyMar=setInterval(Marquee,speed)--}}
+			{{--demo.onmouseover=function(){clearInterval(MyMar)}--}}
+			{{--demo.onmouseout=function(){MyMar=setInterval(Marquee,speed)}--}}
+		{{--}--}}
+	{{--</script>--}}
 </head>
 <body>
 @yield('content')
