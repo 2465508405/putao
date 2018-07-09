dc6972007a3c5e01220bd57013a917bdffedd3bb
ykk <ykk@iZ2ze9ca3c1ri016fi6l51Z.(none)>
2018-07-09 21:29:09 +0800

    commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,13 +13,13 @@ $categories = Category::where('base_id',1)->where('id','!=',$firstCategory->id)-
 <div class="header clearfix">
     <div class="w">
         <div class="logo">
-            <a href=""><img src="/images/logo.png" alt="马陆葡萄网" title="马陆葡萄网"></a>
+            <a href=""><img src="/images/logo.png" alt="马陆葡萄网" ></a>
         </div>
         <div class="logo-left">
-            <a href=""><img src="/images/luntan_bg.png" alt="马陆葡萄网" title="马陆葡萄网"></a>
+            <a href=""><img src="/images/luntan_bg.png" alt="马陆葡萄网" ></a>
         </div>
         <div class="logo-right">
-            <a href=""><img style="width:100px;height:100px;" src="/images/weixin.png" alt="马陆葡萄网" title="马陆葡萄网"></a>
+            <a href=""><img style="width:100px;height:100px;" src="/images/weixin.png" alt="马陆葡萄网"></a>
         </div>
     </div>
 </div>
